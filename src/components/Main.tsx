0fd6da91dd82560f6488211ee4c5c25490575540
vianney-herve <105049233+vianney-herve@users.noreply.github.com>
2024-03-30 23:31:12 +0100

    typed a bit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,31 +1,24 @@
 import styles from '../App.module.css';
-import { createSignal, type Component } from 'solid-js';
+import { createSignal } from 'solid-js';
 import Editor from './Editor';
 import Vr from './Vr';
 
-const Main: Component = () => {
-    let [x1, setx1] = createSignal<number>(200)
-    let [x2, setx2] = createSignal<number>(200)
+export default function () {
+  let [x1, setx1] = createSignal<number>(200);
+  let [x2, setx2] = createSignal<number>(200);
 
-    let ref:HTMLDivElement
+  let ref: HTMLDivElement|undefined;
 
-    return (
-      <div ref={ref} class={styles.main}>
-
-        <div style={"width :" + x1().toString() + "px;"}></div>
-
-        <Vr max={() => {return 200}} getX={x1} setX={setx1}></Vr>
-
-        <div style={"width :" + x2().toString() + "px;"}>
-          <Editor></Editor>
-        </div>
-        
-        <Vr getX={x2} max={() => {return ref.clientWidth - x1() - 24 }} setX={setx2}></Vr>
-
-        <div style={"width : calc(100% - 24px - " + (x2()+x1()).toString() + "px);"} >
-        </div>
+  return (
+    <div ref={ref!} class={styles.main}>
+      <div style={"width :" + x1().toString() + "px;"}></div>
+      <Vr max={() => { return 200 }} getX={x1} setX={setx1}></Vr>
+      <div style={"width :" + x2().toString() + "px;"}>
+        <Editor></Editor>
       </div>
-    );
-  };
-
-export default Main
+      <Vr getX={x2} max={() => { return ref!.clientWidth - x1() - 24 }} setX={setx2}></Vr>
+      <div style={"width : calc(100% - 24px - " + (x2() + x1()).toString() + "px);"} >
+      </div>
+    </div>
+  );
+}
--- src/components/Vr.tsx
+++ src/components/Vr.tsx
@@ -1,38 +1,38 @@
-import { addEventListener } from 'solid-js/web';
-import styles from '../App.module.css';
-import type { Component } from 'solid-js';
+import type { Accessor, Setter } from 'solid-js';
 
-const Vr: Component = (props) => {
+interface IVRProps {
+    max?: () => number;
+    min?: () => number;
 
-    function move(e: PointerEvent){
-        props.setX( Math.min( Math.max( min() , props.getX() - ( px - e.screenX ) ), max()) )
-        px = e.screenX
+    getX: Accessor<number>;
+    setX: Setter<number>;
+}
+
+export default function(props: IVRProps) {
+    function move(e: PointerEvent) {
+        props.setX( Math.min( Math.max( min() , props.getX() - ( px - e.screenX ) ), max()) );
+        px = e.screenX;
     }
 
-    function start(e : PointerEvent){
-        window.onpointermove = move
-        window.onpointerup = stop
-        px = e.screenX
+    function start(e: PointerEvent) {
+        window.onpointermove = move;
+        window.onpointerup = stop;
+        px = e.screenX;
     }
 
-    function stop(){
-        window.onpointerup = () => {}
-        window.onpointermove = () => {}
+    function stop() {
+        window.onpointerup = () => {};
+        window.onpointermove = () => {};
     }
 
-    let px = 0
+    let px = 0;
     
-    let min = () => {return 0}
-    let max = () => {return Infinity}
-
-    if (props.min) {min = props.min}
-    if (props.max) {max = props.max}
+    let min = props.min ? props.min : () => { return 0; };
+    let max = props.max ? props.max : () => { return Infinity; }
 
     return (
         <div draggable="false" style="cursor:ew-resize;height:100%;background-color:rgba(255,255,255,0.1); width:12px;min-width:12px;position:relative;" onpointerdown={start}>
             <i draggable="false" style="top:50%;left:50%;position:absolute;transform:translate(-50%);font-size:18px; color:rgba(255,255,255,0.5);" class="ph ph-dots-six-vertical"></i>
         </div>
     );
-};
-
-export default Vr
+}
