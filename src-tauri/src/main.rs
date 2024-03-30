b3e73ee770460d8d36a88d2ca03f535219d73d45
Vianney HERVÉ <vianney.herve@etudiant.univ-rennes.fr>
2024-03-30 19:42:38 +0100

    removed useless commands in main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,9 @@
 #![cfg_attr(all(not(debug_assertions)), windows_subsystem = "windows")]
 
-// Our Tauri Command
-#[tauri::command]
-fn return_string(word: String) -> String{
-    // debugging message
-    println!("The frontend sent us this argument {}", word);
-    return word
-}
-
 fn main() {
   tauri::Builder::default()
     // Register Command with Tauri App
     .invoke_handler(tauri::generate_handler![return_string])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
-}
-
-mod world;
-mod fonts;
-mod packages;
-
-/// Compile Typst Project
-#[tauri::command]
-fn compile_typst() {
-  
 }
