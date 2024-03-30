979ed6a9aacaed5d406994c7e722ae433672a5f1
Tom Chauvel <tom.chauvel@proton.me>
2024-03-30 18:22:50 +0100

    des truc (#2)
    Co-authored-by: Tol0kk <titouan.le.dilavrec@gmail.com>

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import styles from '../App.module.css';
+import type { Component } from 'solid-js';
+
+const Sidebar: Component = () => {
+    return (
+        <div class={styles.sidebar} >
+        </div>
+    );
+};
+
+export default Sidebar
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,11 @@
+import styles from '../App.module.css';
+import type { Component } from 'solid-js';
+
+const Toolbar: Component = () => {
+    return (
+        <div class={styles.toolbar} >
+        </div>
+    );
+  };
+
+export default Toolbar
