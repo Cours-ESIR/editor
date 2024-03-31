a3e56d0123fab0d13f5c47b2f905c8a80289f6af
Tom Chauvel <tom.chauvel@proton.me>
2024-04-01 01:00:56 +0200

    editor auto

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import styles from '../App.module.css';
-import { createSignal } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import Editor from './Editor';
 import Vr from './Vr';
 import { invoke } from '@tauri-apps/api';
@@ -10,25 +10,33 @@ export default function () {
 
   let ref: HTMLDivElement | undefined;
 
-  invoke("compile_project").then();
+  
   let [image, setImage] = createSignal<string>("");
-  let render: Promise<string> = invoke("render_project", { page: 0 })
-  render.then((reponse) => {
-    setImage(reponse)
-  })
+
+
+  let render = ()=>{
+    invoke("compile_project").then();
+
+    let render: Promise<string> = invoke("render_project", { page: 0 })
+    render.then((reponse) => {
+      setImage(reponse)
+    })
+  }
+
+  render()
 
   type Reponse = {
     message: string;
   }
 
-  console.log(image)
+  
 
   return (
     <div ref={ref!} class={styles.main}>
       <div style={"width :" + x1().toString() + "px;"}></div>
       <Vr max={() => { return 200 }} getX={x1} setX={setx1}></Vr>
       <div style={"width :" + x2().toString() + "px;"}>
-        <Editor></Editor>
+        <Editor renderer={render}></Editor>
       </div>
       <Vr getX={x2} max={() => { return ref!.clientWidth - x1() - 24 }} setX={setx2}></Vr>
       <div style={"background: white; width : calc(100% - 24px - " + (x2() + x1()).toString() + "px);"} >
