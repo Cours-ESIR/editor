e661184fc75f596711e0023a34106516506bf500
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-07 23:55:34 +0200

    better style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,17 @@
   import Editor from "$lib/components/editor.svelte";
   import * as Resizable from "$lib/components/ui/resizable/index.js";
   import { invoke } from "@tauri-apps/api";
+  import { ModeWatcher } from "mode-watcher";
 
   let data = "";
 </script>
 
-<Resizable.PaneGroup direction="horizontal" class="w-full h-full">
+<Resizable.PaneGroup direction="horizontal" class="w-full h-dvh">
   <Resizable.Pane defaultSize={50}>
     <Editor path="" bind:uri={data}></Editor>
   </Resizable.Pane>
-  <Resizable.Handle />
-  <Resizable.Pane defaultSize={50}>
-    <img src={data} />
+  <Resizable.Handle withHandle />
+  <Resizable.Pane defaultSize={50} class="overflow-auto h-dvh">
+      <img src={data} />
   </Resizable.Pane>
 </Resizable.PaneGroup>
