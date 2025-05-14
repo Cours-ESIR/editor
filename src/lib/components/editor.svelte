66ef15a870d8e71c8c26bbd972ab725abf2f5fb6
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-14 14:32:05 +0200

    step 1 - done
    just have a note app
    next step : split the view and the model

--- src/lib/components/editor.svelte
+++ src/lib/components/editor.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-  import * as monaco from "monaco-editor";
-
-  import { fs } from "@tauri-apps/api";
-  import { onMount } from "svelte";
-
   let {
     path,
     text = $bindable(),
@@ -11,38 +6,24 @@
     path: string;
     text: string;
   } = $props();
-
-  async function initEditor(div: HTMLDivElement, path: string | undefined) {
-    if (path) {
-      let buff = await fs.readBinaryFile(path);
-      let buff2 = buff as unknown as number[];
-      text = String.fromCharCode.apply(null, buff2);
-    }
-
-    let editor = monaco.editor.create(div, {
-      theme: window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "vs-dark"
-        : "vs",
-      minimap: {
-        enabled: false,
-      },
-      automaticLayout: true,
-      value: text,
-    });
-    editor.onDidChangeModelContent(async (event) => {
-      text = editor.getValue();
-    });
-  }
-
-  let div: HTMLDivElement;
-  onMount(() => {
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", (e) => {
-        monaco.editor.setTheme(e.matches ? "vs-dark" : "vs");
-      });
-    initEditor(div, path);
-  });
 </script>
 
-<div class="h-dvh" bind:this={div}></div>
+<div class="h-dvh p-4 editor" bind:innerText={text} contenteditable>
+  <div>Initial content</div>
+</div>
+<style>
+  .editor {
+    counter-reset: section;
+  }
+  .editor > *::before {
+    counter-increment: section;
+    content: counter(section);
+    position: absolute;
+    left: 1rem;
+    color: #666;
+  }
+  .editor > * {
+    position: relative;
+    padding-left: 8rem;
+  }
+</style>
